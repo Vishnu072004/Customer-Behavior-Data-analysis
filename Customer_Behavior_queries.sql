26f7b774a683e7f1ccbb8daee9166d1c31266425
Vishnu072004 <146358001+Vishnu072004@users.noreply.github.com>
2025-12-04 18:35:11 +0530

    Add files via upload

---
+++ Customer_Behavior_queries.sql
@@ -0,0 +1,73 @@
+select * from customer limit 30
+
+select gender , SUM(purchase_amount) as revenue
+from customer
+group by gender
+
+select customer_id , purchase_amount
+from customer
+where discount_applied = 'Yes' and purchase_amount >= (select AVG(purchase_amount) from customer)
+
+select item_purchased , ROUND(AVG(review_rating :: numeric),2) as "Average Product Rtaing"
+from customer
+group by item_purchased
+order by avg(review_rating) desc
+limit 5
+
+select shipping_type, AVG(purchase_amount)
+from customer
+where shipping_type in ('Standard' , 'Express')
+group by shipping_type
+
+select subscription_status, COUNT(customer_id) as total_customers,
+ROUND(AVG(purchase_amount),2) as avg_spend , ROUND(SUM(purchase_amount),2) as total_revenue
+from customer
+group by subscription_status
+order by total_revenue , avg_spend desc
+
+select item_purchased, ROUND(100 * SUM(CASE WHEN discount_applied = 'Yes' THEN 1 ELSE 0 END)/COUNT(*) , 2) as discount_rate
+from customer
+group by item_purchased
+order by discount_rate desc
+limit 5
+
+with customer_type as (
+select customer_id , previous_purchases,
+CASE 
+	WHEN previous_purchases = 1 THEN 'New'
+	WHEN previous_purchases BETWEEN 2 AND 10 THEN 'Returning'
+	ELSE 'Loyal'
+	END AS customer_segment
+from customer
+)
+
+select customer_segment , count(*) as "Number of Customers"
+from customer_type
+group by customer_segment
+
+
+ WITH item_counts AS (
+    SELECT category,
+           item_purchased,
+           COUNT(customer_id) AS total_orders,
+           ROW_NUMBER() OVER (PARTITION BY category ORDER BY COUNT(customer_id) DESC) AS item_rank
+    FROM customer
+    GROUP BY category, item_purchased
+)
+SELECT item_rank,category, item_purchased, total_orders
+FROM item_counts
+WHERE item_rank <=3
+
+SELECT subscription_status,
+       COUNT(customer_id) AS repeat_buyers
+FROM customer
+WHERE previous_purchases > 5
+GROUP BY subscription_status;
+
+SELECT 
+    age_group,
+    SUM(purchase_amount) AS total_revenue
+FROM customer
+GROUP BY age_group
+ORDER BY total_revenue desc;
+ 
